64a768b29dba4c657fc1b6969b7031a5775b6a71
CatInTheCage <42236164+CatInTheCage@users.noreply.github.com>
2018-08-26 17:34:13 +0800

    extract method in HtmlStatement,TextStatement for templating

--- src/main/java/rentalstore/HtmlStatement.java
+++ src/main/java/rentalstore/HtmlStatement.java
@@ -3,15 +3,33 @@ import java.util.Enumeration;
 public class HtmlStatement {
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
-        while (rentals.hasMoreElements()) {
+        String result = headerString(aCustomer);
+        while(rentals.hasMoreElements()){
             Rental each = (Rental) rentals.nextElement();
-            result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+            //show figures for this rental
+            result += eachRentalString(each);
         }
         //add footer lines
-        result += "<P>You owe<EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-                "</EM> frequent renter points<P>";
+        result +=footerString(aCustomer);
         return result;
+    }
+    String headerString(Customer aCustomer) {
+        return "<H1>Rentals for <EM>"
+                + aCustomer.getName()
+                + "</EM></H1><P>\n";
+    }
+    String eachRentalString (Rental aRental) {
+        return aRental.getMovie().getTitle()
+                + ": "
+                + String.valueOf(aRental.getCharge())
+                + "<BR>\n";
+
+    }
+    String footerString (Customer aCustomer) {
+        return "<P>You owe<EM>" + String.valueOf(aCustomer.getTotalCharge())
+                + "</EM><P>\n"
+                + "On this rental you earned <EM>"
+                + String.valueOf(aCustomer.getTotalFrequentRenterPoints())
+                + "</EM> frequent renter points<P>";
     }
 }
